94afb33449f45c6fdd783268294fba8fb2ca05c4
Putzi San <putzisan@putzisan.de>
2018-05-29 20:40:25 +0200

    start

---
+++ packages/cra-mui-starter/src/components/App.jsx
@@ -0,0 +1,30 @@
+import { CssBaseline, Typography } from '@material-ui/core';
+import React from 'react';
+import styled from 'styled-components';
+import JssAppProvider from './JssAppProvider';
+
+const Fancy = styled.div`
+  display: flex;
+  height: 100vh;
+  background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+`;
+
+const Headline = styled(Typography)`
+  color: black;
+  font-size: 24rem;
+  margin: auto;
+`;
+
+const App = () => (
+  <JssAppProvider>
+    <CssBaseline />
+
+    <Fancy>
+      <Headline variant="headline">
+        42
+      </Headline>
+    </Fancy>
+  </JssAppProvider>
+);
+
+export default App;
---
+++ packages/cra-mui-starter/src/components/JssAppProvider.jsx
@@ -0,0 +1,20 @@
+import { create } from 'jss';
+import { createGenerateClassName, jssPreset } from '@material-ui/core';
+import React from 'react';
+import JssProvider from 'react-jss/lib/JssProvider';
+
+const generateClassName = createGenerateClassName();
+const jss = create(jssPreset());
+// We define a custom insertion point that JSS will look for injecting the styles in the DOM.
+// so styled-components will work correctly (see https://material-ui.com/customization/css-in-js/#css-injection-order)
+jss.options.insertionPoint = document.getElementById('jss-insertion-point');
+
+const JssAppProvider = props => (
+  <JssProvider jss={jss} generateClassName={generateClassName}>
+    <React.Fragment>
+      {props.children}
+    </React.Fragment>
+  </JssProvider>
+);
+
+export default JssAppProvider;
